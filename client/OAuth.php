7581e655bb329fd7049c2725def5afc0fc159987
Samuel GUENIER <drakelyx@gmail.com>
2022-06-21 18:20:17 +0200

    adding OAuth constructor

--- client/OAuth.php
+++ client/OAuth.php
@@ -2,6 +2,17 @@
 class OAuth
 {
 
-    
+    public $client_id;
+    public $client_secret;
+    public $redirect_uri;
+    public $callback_uri;
+
+    public function __construct($client_id, $client_secret, $redirect_uri, $callback_uri)
+    {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+        $this->redirect_uri = $redirect_uri;
+        $this->callback_uri = $callback_uri;
+    }
 
 }
